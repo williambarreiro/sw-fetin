a53fcd30202f12e735ca74fdd54274b86ae78f3a
william <williamfraga@gec.inatel.br>
2019-06-03 19:57:20 -0300

    Cadastro do usuario e server.js
    Signed-off-by: william <williamfraga@gec.inatel.br>

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log('Hello World!')
+const express = require('express')
+const app = express()
+
+app.listen(3000,function() {
+    console.log('port 3000')
+})
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+    res.send('cadastro_usuario.ejs')
+})
